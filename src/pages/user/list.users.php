6f942d7e7753cc43f5647aa50d4607e7ccbd675b
Alison-Rodrigues <alison.si1302@gmail.com>
2022-09-19 22:16:57 -0300

    ADD styles

--- src/pages/user/list.users.php
+++ src/pages/user/list.users.php
@@ -1,4 +1,5 @@
 <?php
+    require_once './src/pages/partials/header.php';
     require_once './connect.php';
     require_once './src/database/db.users.php';
 
@@ -7,11 +8,29 @@
 
 
 <main class="card-container">
+    <a href="" class="button-create">Adicionar</a>
     <?php foreach($users as $user): ?>
     <article class="card">
-        <span><?= $user['nome'] ?></span>
-        <span><?= $user['cpf'] ?></span>
-        <span><?= $user['tel'] ?></span>
-    </article>
+        <div class="info-user">
+            <span class="header">Nome</span>
+            <span><?= $user['nome'] ?></span>
+        </div>
+        <div class="info-user">
+            <span class="header">Cpf</span>
+            <span><?= $user['cpf'] ?></span>
+        </div>
+        <div class="info-user">
+            <span class="header">Contato</span>
+            <span><?= $user['tel'] ?></span>
+        </div>
+        <div class="button">
+            <a href=""><img src="/assets/images/botao-editar.png" alt="Ícone de edição" class="icon"></a>
+        </div>
+        <div class="button">
+            <a href=""><img src="/assets/images/excluir_icon.png" alt="Ícone para excluir" class="icon"></a>
+        </div>
+        
+        
+    </article><!--card-->
     <?php endforeach; ?>
-</main>
+</main><!--card-container-->
