b82902a2bad00226071155529a19ada903bbb25a
Alison-Rodrigues <alison.si1302@gmail.com>
2022-09-22 20:21:39 -0300

    Try Catch

--- src/database/db.users.php
+++ src/database/db.users.php
@@ -12,41 +12,67 @@ function listUsersDb($pdo) {
 
 
 function createUserDb($pdo, $nome, $cpf, $tel) {
-    $sql = "INSERT INTO users (nome, cpf, tel) VALUES (:nome, :cpf, :tel)";
+    try {
+        $sql = "INSERT INTO users (nome, cpf, tel) VALUES (:nome, :cpf, :tel)";
 
-    $statement = $pdo -> prepare($sql);
+        $statement = $pdo->prepare($sql);
 
-    
+        if($statement->execute([
+            ':nome' => $nome,
+            ':cpf' => $cpf,
+            ':tel' => $tel,
+        ])) {
+            $message = 'success-create';
+        }
 
-    $message = $statement->execute([
-        ':nome' => $nome,
-        ':cpf' => $cpf,
-        ':tel' => $tel,
-    ]) ? 'success-create' : 'error-create';
-    return header("location: ./list.users.php?message=$message");
+        
+
+    } catch (\Throwable) {
+        $message = 'error-create';
+        return header("location: ./list.users.php?message=$message");
+        
+    }
 }
 
 function deleteUserDb($pdo, $id) {
-    $sql = "DELETE FROM users WHERE id = :id";
 
-    $statement = $pdo -> prepare($sql);
+    try {
+        $sql = "DELETE FROM users WHERE id = :id";
 
-    $statement -> bindParam(':id', $id, PDO::PARAM_INT);
+        $statement = $pdo -> prepare($sql);
 
-    $message = $statement -> execute() ? 'success-delete' : 'error-delete';
-    return header("location: ./list.users.php?message=$message");
+        $statement -> bindParam(':id', $id, PDO::PARAM_INT);
+
+        if($statement->execute()) {
+            $message = 'success-delete';
+        }
+
+        return header("location: ./list.users.php?message=$message");
+            
+    } catch (\Throwable) {
+        $message = 'error-delete';
+        return header("location: ./list.users.php?message=$message");
+    }
 }
 
 function updateUserDb($pdo, $id, $nome, $cpf, $tel) {
-    $sql = "UPDATE users SET nome = :nome, cpf = :cpf, tel = :tel WHERE id = :id";
+    try {
+        $sql = "UPDATE users SET nome = :nome, cpf = :cpf, tel = :tel WHERE id = :id";
 
-    $statement = $pdo -> prepare($sql);
+        $statement = $pdo -> prepare($sql);
 
-    $statement -> bindParam(':nome', $nome, PDO::PARAM_STR);
-    $statement -> bindParam(':cpf', $cpf, PDO::PARAM_STR);
-    $statement -> bindParam(':tel', $tel, PDO::PARAM_STR);
-    $statement -> bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->bindParam(':nome', $nome, PDO::PARAM_STR);
+        $statement->bindParam(':cpf', $cpf, PDO::PARAM_STR);
+        $statement->bindParam(':tel', $tel, PDO::PARAM_STR);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
 
-    $message = $statement -> execute() ? 'success-update' : 'error-update';
-    return header("location: ./list.users.php?message=$message");
+        if($statement->execute()) {
+            $message = 'success-update';
+            return header("location: ./list.users.php?message=$message");
+        }
+    } catch (\Throwable) {
+        $message = 'error-update';
+        return header("location: ./list.users.php?message=$message");
+        
+    }
 }
